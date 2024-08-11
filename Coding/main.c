358a29e8a4bdcd219c33a374edcdbb3961b738b2
Valérian Grégoire--Bégranger <valerian.greg@gmail.com>
2024-08-11 22:51:01 +0200

    Finished PCB Schematic

---
+++ Coding/main.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+
+
+// I2C Standard mode
+#define I2C_FREQ 100 * 1000 // Uncomment to use in the code
+// I2C Fast mode
+// #define I2C_FREQ 400 * 1000 // Uncomment to use in the code
+// I2C Fast mode+
+// #define I2C_FREQ 1000 * 1000 // Uncomment to use in the code
+
+#define 
+
+
+uint32_t user_time_secs; // User defined time
+
+// Current time, starts at user_time_secs, and is updated every second
+uint32_t day_time_secs;
+
+
+
+int main(){
+
+    return 0;
+}
